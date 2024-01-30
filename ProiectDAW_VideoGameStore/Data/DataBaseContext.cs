6fead17589771705902c3a91d57e31d60f2e5ca4
ciprianhutanu <eusebiuciprian.hutanu03@gmail.com>
2024-01-30 13:29:52 +0200

    DataBase Context created.

---
+++ ProiectDAW_VideoGameStore/Data/DataBaseContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProiectDAW_VideoGameStore.Models;
+
+namespace ProiectDAW_VideoGameStore.Data
+{
+    public class DataBaseContext:DbContext
+    {
+        public DbSet<Order> Orders {  get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<StoreItem> Items { get; set; }
+        public DbSet<PlacedOn> MMRelations {  get; set; }
+
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(us => us.Orders)
+                .WithOne(or => or.User);
+
+            modelBuilder.Entity<User>()
+                .HasMany(us => us.Reviews)
+                .WithOne(rv => rv.User);
+
+            modelBuilder.Entity<StoreItem>()
+                .HasMany(si => si.Reviews)
+                .WithOne(rv => rv.StoreItem);
+
+            modelBuilder.Entity<PlacedOn>().HasKey(po => new { po.StoreItemId, po.OrderId });
+
+            modelBuilder.Entity<PlacedOn>()
+                .HasOne(po => po.Order)
+                .WithMany(or => or.ItemsPlacedOn)
+                .HasForeignKey(po => po.OrderId);
+
+            modelBuilder.Entity<PlacedOn>()
+                .HasOne(po => po.StoreItem)
+                .WithMany(si => si.PlacedOnOrders)
+                .HasForeignKey(po => po.StoreItemId);
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
