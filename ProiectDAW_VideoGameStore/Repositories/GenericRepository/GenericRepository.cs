bf92cac5ea71cd952be8804705fb7df7f8323b8c
ciprianhutanu <eusebiuciprian.hutanu03@gmail.com>
2024-01-30 14:44:41 +0200

    Adds Generic Repository.

---
+++ ProiectDAW_VideoGameStore/Repositories/GenericRepository/GenericRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProiectDAW_VideoGameStore.Data;
+using ProiectDAW_VideoGameStore.Models.Generic;
+
+namespace ProiectDAW_VideoGameStore.Repositories.GenericRepository
+{
+    public class GenericRepository<TEntity>:IGenericRepository<TEntity> where TEntity : GenericBase
+    {
+        protected readonly DataBaseContext _dbContext;
+        protected readonly DbSet<TEntity> _table;
+
+        public async Task<List<TEntity>> GetAllAsync()
+        {
+            return await _table.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(Guid id)
+        {
+            return await _table.AsNoTracking().FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task CreateAsync(TEntity entity)
+        {
+            await _table.AddAsync(entity);
+        }
+
+        public async Task CreateRangeAsync(IEnumerable<TEntity> entities)
+        {
+            await _table.AddRangeAsync(entities);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _table.Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            _table.UpdateRange(entities);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _table.Remove(entity);
+        }
+
+        public void DeleteById(Guid id)
+        {
+            var entity = _table.Find(id);
+            if (entity != null)
+            {
+                return;
+            }
+            _table.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<TEntity> entities)
+        {
+            _table.RemoveRange(entities);
+        }
+
+        public async Task<TEntity> FindByIdAsync(Guid id)
+        {
+            return await _table.FindAsync(id);
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _dbContext.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ ProiectDAW_VideoGameStore/Repositories/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProiectDAW_VideoGameStore.Models.Generic;
+
+namespace ProiectDAW_VideoGameStore.Repositories.GenericRepository
+{
+    public interface IGenericRepository<TEntity> where TEntity : GenericBase
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task CreateAsync(TEntity entity);
+        Task CreateRangeAsync(IEnumerable<TEntity> entities);
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
+        void Delete(TEntity entity);
+        void DeleteById(Guid id);
+        void DeleteRange(IEnumerable<TEntity> entities);
+        Task<TEntity> FindByIdAsync(Guid id);
+        Task<bool> SaveAsync();
+    }
+}
