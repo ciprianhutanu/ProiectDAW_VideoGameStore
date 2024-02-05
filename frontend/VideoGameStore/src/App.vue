5a5c98ebdfaca5b3ab61d31606789c5495f6a81e
ciprianhutanu <eusebiuciprian.hutanu03@gmail.com>
2024-02-05 19:16:19 +0200

    Installed vue.js

---
+++ frontend/VideoGameStore/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from "@/views/Login.vue";
+
+export default {
+  name: "App",
+  components: {
+    Login
+  }
+};
+</script>
+
+<style>
+/* Add your global styles here if needed */
+</style>
